3aa85064a8f311527b2c377d70c2f7e84eac551c
Shriphani Palakodety <shriphanip@gmail.com>
2014-02-11 14:44:16 -0500

    added enlive dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.7.9"]
                  [com.github.kyleburton/clj-xpath "1.4.3"]
+                 [enlive "1.1.1"]
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]
                  [org.clojure/clojure "1.5.1"]])
